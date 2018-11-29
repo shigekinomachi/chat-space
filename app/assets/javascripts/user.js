e5db3bb606e9f93d6fa3439886cb39bea167404e
Shigeki Nomachi <shigekinomachi@gmail.com>
2018-11-29 13:12:28 +0900

    Incremental search
    ご指摘いただきました点、修正しました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -22,7 +22,7 @@ $(function() {
 
   function appendMember(id, name){
     var html = `
-      <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <div class='chat-group-user clearfix js-chat-member'>
         <input name='group[user_ids][]' type='hidden' value="${id}">
         <p class='chat-group-user__name'>${name}</p>
         <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
